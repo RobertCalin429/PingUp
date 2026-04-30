ade4f56d72c6e827d8e9a374d111474c8991232f
Robert <robertcalin429@gmail.com>
2026-05-01 00:33:36 +0300

    Fix Inngest

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -7,8 +7,7 @@ export const inngest = new Inngest({ id: "pingup-app" });
 //Inngest function to save a user in database
 const syncUserCreation = inngest.createFunction(
     
-    {id: 'sync-user-from-clerk',
-    event: 'clerk/user.created'},
+   { id: 'sync-user-from-clerk', event: 'clerk/user.created' },
     async ({event}) => {
         const {id, first_name, last_name, email_addresses, image_url}=event.data
         let username = email_addresses[0].email_address.split('@')[0]
