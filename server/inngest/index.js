84ffa2de942f6341f7ed49f9719f2598727d360f
Robert <robertcalin429@gmail.com>
2026-05-15 20:30:47 +0300

    Trigger sendNotificationOfUnseenMessages rezolvat

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -135,7 +135,7 @@ const deleteStory = inngest.createFunction(
 const sendNotificationOfUnseenMessages = inngest.createFunction(
   {
     id: "send-unseen-messages-notification",
-    cron: "TZ=Europe/Bucharest 0 9 * * *",
+    triggers: [{ cron: "TZ=Europe/Bucharest 0 9 * * *" }],
   }, //Every Day 9 AM
   async ({ step }) => {
     const messages = await Message.find({ seen: false }).populate("to_user_id");
